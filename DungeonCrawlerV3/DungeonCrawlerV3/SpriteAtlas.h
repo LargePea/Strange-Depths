48c6f80081dd6a2feb5978f641c880fda71a51fc
Ralph Li <360ralphli96@gmail.com>
2023-02-21 00:17:54 -0800

    orgainzed sprite files as well as added a sprite atlas to make changing file paths easier

---
+++ DungeonCrawlerV3/DungeonCrawlerV3/SpriteAtlas.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#pragma region Menus
+
+constexpr auto MENU_BASE =      "Sprites\\Menus\\MenuBackGround.txt";
+constexpr auto COMBAT_MENU =    "Sprites\\Menus\\CombatMenu.txt";
+constexpr auto INVENTORY_MENU = "Sprites\\Menus\\InventoryMenu.txt";
+constexpr auto OVERFLOW_MENU =  "Sprites\\Menus\\OverflowMenu.txt";
+constexpr auto MOVEMENT_MENU =  "Sprites\\Menus\\MovementMenu.txt";
+constexpr auto TREASURE_MENU =  "Sprites\\Menus\\TreasureMenu.txt";
+constexpr auto SHOP_MENU =		"Sprites\\Menus\\ShopMenu.txt";
+constexpr auto MAIN_MENU =      "Sprites\\Menus\\MainMenu.txt";
+
+#pragma endregion
+
+#pragma region Characters
+
+
+
+#pragma endregion
+
+#pragma region BackGrounds
+
+constexpr auto BASIC_BACKGROUND = "Sprites\\BackGround.txt";
+
+#pragma endregion
