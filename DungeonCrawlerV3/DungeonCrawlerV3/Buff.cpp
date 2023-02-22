65eb7eb6e23e5d7af3cb45ce5ebb876c848029ea
Ralph Li <360ralphli96@gmail.com>
2023-02-21 23:52:30 -0800

    fixed bug with potions where if used when potion is active it doesnt increase buff duration but duplicates buff

--- DungeonCrawlerV3/DungeonCrawlerV3/Buff.cpp
+++ DungeonCrawlerV3/DungeonCrawlerV3/Buff.cpp
@@ -16,6 +16,7 @@ void Buff::TryUseBuff() {
 }
 
 //LifeSteal
+std::vector<LifeSteal*> LifeSteal::_activeLifeStealBuffs;
 
 LifeSteal::LifeSteal(Character* user, int uses, float lifeStealStrength) : 
 	Buff(user, uses, static_cast<int>(GameStateMask::Combat)), _lifeStealStrength(lifeStealStrength) {
@@ -29,17 +30,28 @@ void LifeSteal::UseBuff() {
 	--_usesRemaining;
 
 	Notification lSNotif({
-	".You.used.a.LifeSteal.Buff."
+	".You.used.a.LifeSteal.Buff.",
 	".Uses.Remaining:." + std::to_string(_usesRemaining) + ".",
 	".Press.Any.Key.To.Continue."
 		}, { 42, 38 });
 }
 
 void LifeSteal::NoMoreUses() {
+	DeactivateBuff(this);
 	_user->AttackEvent.Remove(_buffEvent);
 }
 
+void LifeSteal::DeactivateBuff(LifeSteal* buff) {
+	for (auto it = _activeLifeStealBuffs.begin(); it != _activeLifeStealBuffs.end(); ++it)
+		if (buff == *it) {
+			_activeLifeStealBuffs.erase(it);
+			break;
+		}		
+}
+
 //Rengeneration
+std::vector<Regeneration*> Regeneration::_activeRegenerationBuffs;
+
 Regeneration::Regeneration(Character* user, int uses, float totalHealPercent) : 
 	Buff(user, uses, static_cast<int>(GameStateMask::Combat)), _totalHealPercent(totalHealPercent) {
 	_buffEvent = _user->TurnBeginEvent.Attach((Buff*)this, &Buff::TryUseBuff, true);
@@ -53,12 +65,26 @@ void Regeneration::UseBuff() {
 	--_usesRemaining;
 
 	Notification regenNotif({
-		".You.used.a.Regeneration.Buff."
+		".You.used.a.Regeneration.Buff.",
 		".Uses.Remaining:." + std::to_string(_usesRemaining) + ".",
 		".Press.Any.Key.To.Continue."
 		}, { 42, 38 });
 }
 
+void Regeneration::IncreaseUses(int additionalUses) {
+	Buff::IncreaseUses(additionalUses);
+	UseBuff();
+}
+
 void Regeneration::NoMoreUses() {
+	DeactivateBuff(this);
 	_user->TurnBeginEvent.Remove(_buffEvent);
+}
+
+void Regeneration::DeactivateBuff(Regeneration* buff) {
+	for (auto it = _activeRegenerationBuffs.begin(); it != _activeRegenerationBuffs.end(); ++it)
+		if (buff == *it) {
+			_activeRegenerationBuffs.erase(it);
+			break;
+		}
 }
--- DungeonCrawlerV3/DungeonCrawlerV3/Potion.cpp
+++ DungeonCrawlerV3/DungeonCrawlerV3/Potion.cpp
@@ -1,11 +1,25 @@
 #include "Potion.h"
 
 void LifeStealPotion::UseItem(Character* user) {
-	new LifeSteal(user, _buffDuration, _lifeStealStrength);
+	for (auto& buff : LifeSteal::_activeLifeStealBuffs) {
+		if (buff->_user == user) {
+			buff->IncreaseUses(buff->_maxUses);
+			return;
+		}
+	}
+
+	LifeSteal::_activeLifeStealBuffs.push_back(new LifeSteal(user, _buffDuration, _lifeStealStrength));
 }
 
 void RegenPotion::UseItem(Character* user) {
-	new Regeneration(user, _buffDuration, _regenTotalPercentage);
+	for (auto& buff : Regeneration::_activeRegenerationBuffs) {
+		if (buff->_user == user) {
+			buff->IncreaseUses(buff->_maxUses);
+			return;
+		}
+	}
+
+	Regeneration::_activeRegenerationBuffs.push_back(new Regeneration(user, _buffDuration, _regenTotalPercentage));
 }
 
 void HealPotion::UseItem(Character* user) {
