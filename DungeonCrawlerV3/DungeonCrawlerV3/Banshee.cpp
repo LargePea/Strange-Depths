2cbfa7bad68f25e61d2292c97a9276ff05916233
Ralph Li <360ralphli96@gmail.com>
2023-02-23 00:12:44 -0800

    fixed image misplacment with banshee

--- DungeonCrawlerV3/DungeonCrawlerV3/Banshee.cpp
+++ DungeonCrawlerV3/DungeonCrawlerV3/Banshee.cpp
@@ -2,7 +2,7 @@
 #include "ItemDictionary.h"
 
 Banshee::Banshee() :
-	Enemy(12, 8, 0, 0.35f, 7.5f, "Banshee", 30, 0, Image(BANSHEE, 2, { 35, 18 }))
+	Enemy(12, 8, 0, 0.35f, 7.5f, "Banshee", 30, 0, Image(BANSHEE, 2, { 50, 5 }))
 {
 	static ItemDictionary instance = ItemDictionary::Instance();
 	static LootTable bansheeLootTable({
